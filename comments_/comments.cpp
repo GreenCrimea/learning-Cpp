ee70d1721e29573ed4c2e6890541e226a99bb488
greencrimea <tom.horton100@gmail.com>
2022-08-08 11:28:20 +1000

    .

---
+++ comments_/comments.cpp
@@ -0,0 +1,15 @@
+//one line comment 
+
+//enables cout and endl from the iostream library
+#include <iostream>
+
+/*
+multi line comment block
+block comment 
+*/
+
+int main(){
+    //print hello world to the console
+    std::cout << "hello world" << std::endl; 
+    return 0;
+}
